3c05ac116bab9137bc92c8ce49074d70a1ef911a
Philipp König <kingcastle@gmx.net>
2014-05-04 00:06:33 +0200

    added media load pics for cards ... very diiiirty

--- MUW SIP Trainer/MainViewController.m
+++ MUW SIP Trainer/MainViewController.m
@@ -45,6 +45,10 @@
     currentCardIndex = 0;
     currentTag = @"Block01";
     [self setCard];
+    
+    [[_webView scrollView] setBounces:NO];
+    [[_webViewCardBack scrollView] setBounces:NO];
+    
 }
 
 
@@ -84,20 +88,48 @@
         // magic ascii separator used by anki for front and back of the card
         NSArray *frontAndBack = [[resultCard stringForColumn:@"flds"]componentsSeparatedByString:[NSString stringWithFormat:@"%c", 31]];
         
+        NSData *mediaJsonData = [[NSData alloc] initWithContentsOfFile: [[NSBundle mainBundle] pathForResource: @"media" ofType: nil]];
+        NSDictionary *parsedObject = [NSJSONSerialization JSONObjectWithData:mediaJsonData options:0 error:nil];
         
-        //paste-115130893336577.png
-        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        NSString *documentDirectory = [paths objectAtIndex:0];
-        NSString* file= [documentDirectory stringByAppendingPathComponent:@"paste-115130893336577.png"];
-        NSData *data = [[NSData alloc] initWithContentsOfFile: [[NSBundle mainBundle] pathForResource: @"44" ofType: nil]];
-        [data writeToFile:file atomically:YES];
-        NSURL *url = [NSURL fileURLWithPath:documentDirectory];
+        
+        __block NSURL *url;
+        [parsedObject enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+            if ([frontAndBack[0] rangeOfString:obj].location != NSNotFound) {
+                NSData *data = [[NSData alloc] initWithContentsOfFile: [[NSBundle mainBundle] pathForResource: key ofType: nil]];
+                
+                NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+                NSString *documentDirectory = [paths objectAtIndex:0];
+                NSString* file= [documentDirectory stringByAppendingPathComponent:obj];
+                
+                UIImage *image = [UIImage imageWithData:data];
+                
+                //CGRect screenBounds = [[UIScreen mainScreen] bounds];
+                CGSize screenSize = CGSizeMake(150, 150);
+                
+                image = [self scaleImage:image toSize:screenSize];
+                
+                data = UIImagePNGRepresentation(image);
+                
+                
+                [data writeToFile:file atomically:YES];
+                url = [NSURL fileURLWithPath:documentDirectory];
+                
+                *stop = YES;
+            }
+        }];
+        
+        
+
         
         NSString *front = [NSString stringWithFormat:@"<font face='Sans-Serif' size='3'>%@", frontAndBack[0]];
         NSString *back = [NSString stringWithFormat:@"<font face='Sans-Serif' size='3'>%@", frontAndBack[1]];
         
         [_webView loadHTMLString:front baseURL:url];
         [_webViewCardBack loadHTMLString:back baseURL:url];
+        
+        [_webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName('img')[0].style.width = '280px'"];
+        [_webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName('img')[0].style.height = '280px'"];
+
     }
     @catch (NSException *exception)
     {
@@ -150,6 +182,29 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIImage*) scaleImage:(UIImage*)image toSize:(CGSize)newSize {
+    CGSize scaledSize = newSize;
+    float scaleFactor = 1.0;
+    if( image.size.width > image.size.height ) {
+        scaleFactor = image.size.width / image.size.height;
+        scaledSize.width = newSize.width;
+        scaledSize.height = newSize.height / scaleFactor;
+    }
+    else {
+        scaleFactor = image.size.height / image.size.width;
+        scaledSize.height = newSize.height;
+        scaledSize.width = newSize.width / scaleFactor;
+    }
+    
+    UIGraphicsBeginImageContextWithOptions( scaledSize, NO, 0.0 );
+    CGRect scaledImageRect = CGRectMake( 0.0, 0.0, scaledSize.width, scaledSize.height );
+    [image drawInRect:scaledImageRect];
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return scaledImage;
 }
 
 @end
